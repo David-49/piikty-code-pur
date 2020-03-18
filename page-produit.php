2e978e77f7c76abf360ee31825c4ea0a48ec1080
David DOGNIN <dd08.brad@hotmail.fr>
2020-03-18 23:33:44 +0100

    save

--- page-produit.php
+++ page-produit.php
@@ -14,51 +14,64 @@ if (!empty($_GET['cat'])) {
 $sql = "SELECT piikti_produit.id AS id_produit, piikti_users.id AS id_createurs, id_utilisateur, nom, prenom, nom_produit, prix_produit, chemin_photo_produit FROM piikti_users INNER JOIN piikti_produit WHERE piikti_users.id = piikti_produit.id_utilisateur AND sexe_produit = '$sexeProduit'";
 $check = "";
 
+$btnALL = true;
+$btnManteaux = false;
+$btnChemises = false;
+$btnPulls = false;
+$btnTshirts = false;
+$btnPantalons = false;
+$btnAcc = false;
+
 if (!empty($_POST['btnALL'])) {
-    var_dump($_POST['btnALL']);
     $check = true;
 }
 
 if (!empty($_POST['btnManteaux'])) {
-    var_dump($_POST['btnManteaux']);
     $btnManteaux = valid_donnees($_POST['btnManteaux']);
     $sql .= " AND  piikti_produit.cat_produit = '$btnManteaux'";
     $check = false;
+    $btnManteaux = true;
+    $btnALL = false;
 }
 
 if (!empty($_POST['btnChemises'])) {
-    var_dump($_POST['btnChemises']);
     $btnChemises = valid_donnees($_POST['btnChemises']);
     $sql .= " AND  piikti_produit.cat_produit = '$btnChemises'";
     $check = false;
+    $btnChemises = true;
+    $btnALL = false;
 }
 
 if (!empty($_POST['btnPulls'])) {
-    var_dump($_POST['btnPulls']);
     $btnPulls = valid_donnees($_POST['btnPulls']);
     $sql .= " AND  piikti_produit.cat_produit = '$btnPulls'";
     $check = false;
+    $btnPulls = true;
+    $btnALL = false;
 }
 
 if (!empty($_POST['btnTshirts'])) {
-    var_dump($_POST['btnTshirts']);
     $btnTshirts = valid_donnees($_POST['btnTshirts']);
     $sql .= " AND  piikti_produit.cat_produit = '$btnTshirts'";
     $check = false;
+    $btnTshirts = true;
+    $btnALL = false;
 }
 
 if (!empty($_POST['btnPantalons'])) {
-    var_dump($_POST['btnPantalons']);
     $btnPantalons = valid_donnees($_POST['btnPantalons']);
     $sql .= " AND  piikti_produit.cat_produit = '$btnPantalons'";
     $check = false;
+    $btnPantalons = true;
+    $btnALL = false;
 }
 
 if (!empty($_POST['btnAcc'])) {
-    var_dump($_POST['btnAcc']);
     $btnAcc = valid_donnees($_POST['btnAcc']);
     $sql .= " AND  piikti_produit.cat_produit = '$btnAcc'";
     $check = false;
+    $btnAcc = true;
+    $btnALL = false;
 }
 if ($check == true) {
     $sql  = "SELECT piikti_produit.id AS id_produit, piikti_users.id AS id_createurs, id_utilisateur, nom, prenom, nom_produit, prix_produit, chemin_photo_produit FROM piikti_users INNER JOIN piikti_produit WHERE piikti_users.id = piikti_produit.id_utilisateur AND sexe_produit = '$sexeProduit'";
@@ -77,13 +90,27 @@ $reqS -> execute();
         <h1 class="titrePageProduit">CATALOGUE PRODUITS</h1>
 
         <form class="filtreProduit"  method="post">
-            <button type="submit" name="btnAll" value="all">VOIR TOUT</button>
-            <button type="submit" name="btnManteaux" value="manteaux_blousons">MANTEAUX/BLOUSONS</button>
-            <button type="submit" name="btnChemises" value="chemises">CHEMISES</button>
-            <button type="submit" name="btnPulls" value="pull">PULLS</button>
-            <button type="submit" name="btnTshirts" value="t_shirts">T-SHIRTS</button>
-            <button type="submit" name="btnPantalons" value="pantalon">PANTALONS</button>
-            <button type="submit" name="btnAcc" value="accessoires">ACCESSOIRES</button>
+            <button type="submit" name="btnAll" value="all" class="<?php if ($btnALL == true) {
+    echo "cat-rouge";
+} ?>">VOIR TOUT</button>
+            <button type="submit" name="btnManteaux" value="manteaux_blousons" class="<?php if ($btnManteaux == true) {
+    echo "cat-rouge";
+} ?>">MANTEAUX/BLOUSONS</button>
+            <button type="submit" name="btnChemises" value="chemises" class="<?php if ($btnChemises == true) {
+    echo "cat-rouge";
+} ?>">CHEMISES</button>
+            <button type="submit" name="btnPulls" value="pull" class="<?php if ($btnPulls == true) {
+    echo "cat-rouge";
+} ?>">PULLS</button>
+            <button type="submit" name="btnTshirts" value="t_shirts" class="<?php if ($btnTshirts == true) {
+    echo "cat-rouge";
+} ?>">T-SHIRTS</button>
+            <button type="submit" name="btnPantalons" value="pantalon" class="<?php if ($btnPantalons == true) {
+    echo "cat-rouge";
+} ?>">PANTALONS</button>
+            <button type="submit" name="btnAcc" value="accessoires" class="<?php if ($btnAcc == true) {
+    echo "cat-rouge";
+} ?>">ACCESSOIRES</button>
         </form>
     </div>
 
@@ -106,8 +133,7 @@ $reqS -> execute();
                             <a href='page-profile.php?idCrea=".$ligne -> id_createurs."' class='createursProduit'>".$ligne -> prenom." ".$ligne -> nom."</a>
                         </div>
                     </div>
-                </div>
-            ";
+                </div>";
         }
 
         ?>
